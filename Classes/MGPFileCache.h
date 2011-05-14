b52486db21eb488273021f2ccd23e5865badfeda
Saul Mora <saul@casademora.com>
2011-05-14 12:30:21 -0600

    Initial checkin for File Cache object

---
+++ Classes/MGPFileCache.h
@@ -0,0 +1,16 @@
+//
+//  MGPFileCache.h
+//  MGPRemoteAssetController
+//
+//  Created by Saul Mora on 5/14/11.
+//  Copyright 2011 Magical Panda Software LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MGPFileCache : NSObject {
+    
+}
+
+@end
---
+++ Classes/MGPFileCache.m
@@ -0,0 +1,14 @@
+//
+//  MGPFileCache.m
+//  MGPRemoteAssetController
+//
+//  Created by Saul Mora on 5/14/11.
+//  Copyright 2011 Magical Panda Software LLC. All rights reserved.
+//
+
+#import "MGPFileCache.h"
+
+
+@implementation MGPFileCache
+
+@end
